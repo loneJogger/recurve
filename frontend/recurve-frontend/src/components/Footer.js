48b3d74f5d5cf644a4dc14f407e98cf742e287a1
loneJogger <helen.tremarche@gmail.com>
2020-07-12 21:11:06 -0400

    fixed formatting

--- frontend/recurve-frontend/src/components/Footer.js
+++ frontend/recurve-frontend/src/components/Footer.js
@@ -13,14 +13,14 @@ const Footer = () => {
 export default Footer
 
 const FooterBox = styled.div`
-background: #eab4d5;
-border-top: 4px solid #ad3768;
-margin-top: 32px;
-padding: 16px;
-display: flex;
-flex-direction: column;
-align-items: center;
-position: absolute;
-bottom: 0;
-width: 100%;
+  background: #eab4d5;
+  border-top: 4px solid #ad3768;
+  margin-top: 32px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 `
